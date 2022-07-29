df24d2f1752bd6ad4c148d1d5bafbe8dd48fd100
Valentin Barit <valentin.barit@gmail.com>
2022-07-29 19:40:35 +0200

    fix: 🐛 parallelize http requests on Jobs page

--- app/firebase/getJobs.ts
+++ app/firebase/getJobs.ts
@@ -4,12 +4,12 @@ import getAll from "./getAll";
 import parseJob from "./parseJob";
 
 export default async function getJobs() {
-  const seniorityLevels = await getAll("seniorityLevels");
-  const skills = await getAll("skills");
-  const jobs = await getAll(
-    "jobs",
-    where("active", "==", true),
-    orderBy("updatedAt", "desc")
-  );
+  const [seniorityLevels, skills, jobs] = (
+    await Promise.allSettled([
+      getAll("seniorityLevels"),
+      getAll("skills"),
+      getAll("jobs", where("active", "==", true), orderBy("updatedAt", "desc")),
+    ])
+  ).map((result) => (result.status === "fulfilled" ? result.value : []));
   return jobs.map(parseJob({ skills, seniorityLevels }));
 }
