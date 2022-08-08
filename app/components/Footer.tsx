41b3decd134bd48b73431287633e8e2226aacf0c
Valentin Barit <valentin.barit@gmail.com>
2022-08-08 11:43:02 +0200

    refactor: 💡 no duplicate footer nav sections

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,61 +8,82 @@ import NavListItem from "./NavListItem";
 import NavIconList from "./NavIconList";
 import NavIconListItem from "./NavIconListItem";
 
+const socialNav = (
+  <NavIconList>
+    <NavIconListItem
+      to="https://www.linkedin.com/company/ctrlup/"
+      name="Ctrl Up sur LinkedIn"
+      Icon={LinkedIn}
+    />
+    <NavIconListItem
+      to="https://www.instagram.com/ctrlup_team/"
+      name="Ctrl Up Team sur Instagram"
+      Icon={Instagram}
+    />
+    <NavIconListItem
+      to="https://discord.gg/5NPXddG9jx"
+      name="Tech Ctrl sur Discord"
+      Icon={DiscordIcon}
+    />
+    <NavIconListItem
+      to="https://github.com/ctrlup-io"
+      name="Ctrl Up sur GitHub"
+      Icon={GitHub}
+    />
+    <NavIconListItem
+      to="https://www.npmjs.com/org/ctrlup"
+      name="Ctrl Up sur Npm"
+      Icon={NpmIcon}
+    />
+  </NavIconList>
+);
+
+const teamDedication = (
+  <Typography p={4} textAlign="center">
+    Fait avec{" "}
+    <span role="img" aria-label="heart">
+      ❤️
+    </span>{" "}
+    par notre équipe.
+  </Typography>
+);
+
 function Footer() {
   const theme = useTheme();
   const isLarge = useMediaQuery(theme.breakpoints.up("lg"));
+
+  const mainNav = (
+    <NavList fullWidth={!isLarge}>
+      <NavListItem name="À propos" to="/about" />
+      <NavListItem name="Services" to="/services" />
+      <NavListItem name="Équipe" to="/team" />
+      <NavListItem name="Jobs" to="/jobs" />
+      <NavListItem name="Contact" to="/contact" primary />
+    </NavList>
+  );
+
+  const secondaryNav = (
+    <NavList fullWidth={!isLarge}>
+      <NavListItem
+        name="Ctrl Up"
+        external
+        to="https://www.infogreffe.fr/entreprise-societe/888875127-ctrl-up-330220B042110000.html"
+      />
+      <NavListItem name="Mentions légales" to="/privacy-and-terms" />
+    </NavList>
+  );
+
   if (isLarge) {
     return (
       <Box component="nav" bgcolor="background.paper" p={8} display="flex">
-        <NavList fullWidth={!isLarge}>
-          <NavListItem name="À propos" to="/about" />
-          <NavListItem name="Services" to="/services" />
-          <NavListItem name="Équipe" to="/team" />
-          <NavListItem name="Jobs" to="/jobs" />
-          <NavListItem name="Contactez-nous" to="/contact" primary />
-          <NavListItem
-            name="Ctrl Up"
-            external
-            to="https://www.infogreffe.fr/entreprise-societe/888875127-ctrl-up-330220B042110000.html"
-          />
-          <NavListItem name="Mentions légales" to="/privacy-and-terms" />
-        </NavList>
+        <Box display="flex" flexDirection="column">
+          {mainNav}
+          {secondaryNav}
+        </Box>
         <Box flexGrow={1} />
         <Box display="flex" flexDirection="column" justifyContent="flex-end">
-          <NavIconList>
-            <NavIconListItem
-              to="https://www.linkedin.com/company/ctrlup/"
-              name="Ctrl Up sur LinkedIn"
-              Icon={LinkedIn}
-            />
-            <NavIconListItem
-              to="https://www.instagram.com/ctrlup_team/"
-              name="Ctrl Up Team sur Instagram"
-              Icon={Instagram}
-            />
-            <NavIconListItem
-              to="https://discord.gg/5NPXddG9jx"
-              name="Tech Ctrl sur Discord"
-              Icon={DiscordIcon}
-            />
-            <NavIconListItem
-              to="https://github.com/ctrlup-io"
-              name="Ctrl Up sur GitHub"
-              Icon={GitHub}
-            />
-            <NavIconListItem
-              to="https://www.npmjs.com/org/ctrlup"
-              name="Ctrl Up sur Npm"
-              Icon={NpmIcon}
-            />
-          </NavIconList>
-          <Typography>
-            Fait avec{" "}
-            <span role="img" aria-label="heart">
-              ❤️
-            </span>{" "}
-            par notre équipe.
-          </Typography>
+          {socialNav}
+          {teamDedication}
         </Box>
       </Box>
     );
@@ -74,57 +95,10 @@ function Footer() {
       display="flex"
       flexDirection="column"
     >
-      <NavList fullWidth>
-        <NavListItem name="À propos" to="/about" />
-        <NavListItem name="Services" to="/services" />
-        <NavListItem name="Équipe" to="/team" />
-        <NavListItem name="Jobs" to="/jobs" />
-        <NavListItem name="Contact" to="/contact" primary />
-      </NavList>
-      <Box display="flex" flexDirection="column" justifyContent="flex-end">
-        <NavIconList>
-          <NavIconListItem
-            to="https://www.linkedin.com/company/ctrlup/"
-            name="Ctrl Up sur LinkedIn"
-            Icon={LinkedIn}
-          />
-          <NavIconListItem
-            to="https://www.instagram.com/ctrlup_team/"
-            name="Ctrl Up Team sur Instagram"
-            Icon={Instagram}
-          />
-          <NavIconListItem
-            to="https://discord.gg/5NPXddG9jx"
-            name="Tech Ctrl sur Discord"
-            Icon={DiscordIcon}
-          />
-          <NavIconListItem
-            to="https://github.com/ctrlup-io"
-            name="Ctrl Up sur GitHub"
-            Icon={GitHub}
-          />
-          <NavIconListItem
-            to="https://www.npmjs.com/org/ctrlup"
-            name="Ctrl Up sur Npm"
-            Icon={NpmIcon}
-          />
-        </NavIconList>
-      </Box>
-      <NavList fullWidth>
-        <NavListItem
-          name="Ctrl Up"
-          external
-          to="https://www.infogreffe.fr/entreprise-societe/888875127-ctrl-up-330220B042110000.html"
-        />
-        <NavListItem name="Mentions légales" to="/privacy-and-terms" />
-      </NavList>
-      <Typography p={4} textAlign="center">
-        Fait avec{" "}
-        <span role="img" aria-label="heart">
-          ❤️
-        </span>{" "}
-        par notre équipe.
-      </Typography>
+      {mainNav}
+      {socialNav}
+      {secondaryNav}
+      {teamDedication}
     </Box>
   );
 }
